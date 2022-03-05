67c84ede28eee606e50c60300f882ef50f4cf333
salayoshida <yoshidasala@gmail.com>
2022-03-05 03:44:26 -0800

    allwater component

---
+++ client/components/AllWater.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
+
+import mountains, { fetchMountains } from "../store/mountains";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const AllWaters = () => {
+  const mountains = useSelector((state) => state.mountains);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMountains());
+  }, []);
+
+  return (
+    
+    <div className = "chi">
+      <NavBar ></NavBar>
+        <header className= "header">
+        <h1 className = "title">
+          CHI . LAND
+        </h1>
+        
+        <h2  className = "subTitle" >
+          Japanese people have long appreciated the presence of life in all
+          aspects of nature—from landscapes and climates that change
+          seasonally.Their reverence for natural life enables them to coexist
+          with nature.
+        </h2>
+      </header>
+        <div className='allcontainer'>
+          {mountains.length > 0
+            ? mountains.map((mountain) => (
+                <div className='eachcontainer' key={mountain.id}>
+                  <div>
+                    <Link to={`/mountains/${mountain.id}`}>
+                      <img className='eachimages' src={mountain.imageUrl} />
+                    </Link>
+                  </div>
+                </div>
+              ))
+            : "There are no mountains registered in the database"}
+        </div>
+      
+     
+      
+    </div>
+  );
+};
+
+export default AllMountains;
